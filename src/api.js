797a76766fe84116d2a8a117214591122d613616
thesneath <71525393+thesneath@users.noreply.github.com>
2021-08-10 14:17:06 -0700

    added a cached list of events for offline mode

--- src/api.js
+++ src/api.js
@@ -24,6 +24,11 @@ export const getEvents = async () => {
     NProgress.done();
     return mockData;
   }
+  if (!navigator.onLine) {
+    const data = localStorage.getItem('lastEvents');
+    NProgress.done();
+    return data?JSON.parse(events).events:[];;
+  }
   const token = await getAccessToken();
   if (token) {
     removeQuery();
@@ -31,7 +36,7 @@ export const getEvents = async () => {
     const result = await axios.get(url);
     if (result.data) {
       const locations = extractLocations(result.data.events);
-      localStorage.setItem('last events', JSON.stringify(result.data));
+      localStorage.setItem('lastEvents', JSON.stringify(result.data));
       localStorage.setItem('locations', JSON.stringify(locations));
       NProgress.done()
       return result.data.events
