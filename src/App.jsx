f45cb835292537d8aecc4df67e3cd29a48725fbc
thesneath <71525393+thesneath@users.noreply.github.com>
2021-08-10 15:52:47 -0700

    added WelcomeScreen

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,36 @@ import "./nprogress.css";
 import EventList from "./EventList/EventList";
 import CitySearch from "./CitySearch/CitySearch";
 import NumberOfEvents from "./NumberOfEvents/NumberOfEvents";
-import { OfflineAlert } from './Alert'
-import { getEvents, extractLocations } from "./api";
+import WelcomeScreen from "./WelcomeScreen";
+import { OfflineAlert } from "./Alert";
+import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
     numberOfEvents: 20,
-    currentLocation: '',
-    infoText: navigator.onLine ? '' : 'You are not connected to the internet. Some events may not be up to date.'
+    currentLocation: "",
+    infoText: navigator.onLine
+      ? ""
+      : "You are not connected to the internet. Some events may not be up to date.",
+    showWelcomeScreen: undefined,
   };
 
   componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ 
-          events: events, 
-          locations: extractLocations(events),
-         });
-      } 
-    });
+    const accessToken = localStorage.getItem("access_token");
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
   }
 
   updateEvents = (location, eventCount = null) => {
@@ -34,17 +42,20 @@ class App extends Component {
       const locationEvents =
         location === "all"
           ? events.slice(0, numberOfEvents)
-          : events.filter((event) => event.location === location).slice(0, numberOfEvents);
-      if(this.mounted) {
+          : events
+              .filter((event) => event.location === location)
+              .slice(0, numberOfEvents);
+      if (this.mounted) {
         this.setState({
-        events: locationEvents,
-        currentLocation: location,
-      });
-    }});
+          events: locationEvents,
+          currentLocation: location,
+        });
+      }
+    });
   };
 
   updateNumberOfEvents = (number) => {
-    let currentLocation = this.state.currentLocation || 'all'
+    let currentLocation = this.state.currentLocation || "all";
     this.setState({ numberOfEvents: number }, () => {
       this.updateEvents(currentLocation, number);
     });
@@ -55,6 +66,8 @@ class App extends Component {
   }
 
   render() {
+    if (this.state.showWelcomeScreen === undefined)
+      return <div className="App" />;
     return (
       <div className="App">
         <h1>Meet App</h1>
@@ -70,6 +83,12 @@ class App extends Component {
         />
         <OfflineAlert text={this.state.infoText} />
         <EventList events={this.state.events} />
+        <WelcomeScreen
+          showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => {
+            getAccessToken();
+          }}
+        />
       </div>
     );
   }
--- src/api.js
+++ src/api.js
@@ -57,7 +57,7 @@ const getToken = async (code) => {
   return access_token;
 };
 
-const checkToken = async (accessToken) => {
+export const checkToken = async (accessToken) => {
   const result = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`)
     .then(res => res.json())
     .catch(err => err.json());
